d9a25b20535957c721d1d67f7f71008dd6677c9a
xdlewisdx <xdlewisdx@users.noreply.github.com>
2017-10-17 02:21:19 +1100

    Initial commit

---
+++ ServerDetailsEditor.cs
@@ -0,0 +1,60 @@
+﻿using Rocket.Core;
+using Rocket.Core.Logging;
+using Rocket.Core.Plugins;
+using SDG.Unturned;
+using Steamworks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ServerDetailsEditor
+{
+    public class ServerDetailsEditor : RocketPlugin<ServerDetailsEditorConfiguration>
+    {
+        protected override void Load()
+        {
+            Level.onLevelLoaded += Level_onLevelLoaded;
+        }
+
+        protected override void Unload()
+        {
+            Level.onLevelLoaded -= Level_onLevelLoaded;
+        }
+
+        private void Level_onLevelLoaded(int level)
+        {
+            if (Configuration.Instance.largeServer)
+            {
+                if (Provider.maxPlayers > 24)
+                {
+                    SteamGameServer.SetMaxPlayerCount(24);
+                }
+                else
+                {
+                    Logger.Log("Skipping large server due to server count being below 24 players");
+                }
+            }
+
+            if (Configuration.Instance.hideConfiguration)
+            {
+                SteamGameServer.SetKeyValue("Browser_Config_Count", null);
+            }
+
+            if (Configuration.Instance.gameName != null)
+            {
+                SteamGameServer.SetGameTags((!Provider.isPvP ? "PVE" : "PVP") + ",GAMEMODE:" + Configuration.Instance.gameName + ',' + (!Provider.hasCheats ? "STAEHC" : "CHEATS") + ',' + Provider.mode.ToString() + "," + Provider.cameraMode.ToString() + "," + (Provider.serverWorkshopFileIDs.Count <= 0 ? "KROW" : "WORK") + "," + (!Provider.isGold ? "YLNODLOG" : "GOLDONLY") + "," + (!Provider.configData.Server.BattlEye_Secure ? "BATTLEYE_OFF" : "BATTLEYE_ON"));
+            }
+
+            if (Configuration.Instance.hideWorkshop)
+            {
+                SteamGameServer.SetKeyValue("Browser_Workshop_Count", null);
+            }
+
+            if (Configuration.Instance.rocketPlugins != null)
+            {
+                SteamGameServer.SetKeyValue("rocketplugins", string.Join(",", Configuration.Instance.rocketPlugins.ToArray()));
+            }
+        }
+    }
+}
---
+++ ServerDetailsEditorConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Rocket.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ServerDetailsEditor
+{
+    public class ServerDetailsEditorConfiguration : IRocketPluginConfiguration
+    {
+        public bool largeServer;
+        public string gameName;
+        public bool hideWorkshop;
+        public bool hideConfiguration;
+        public List<string> rocketPlugins;
+
+        public void LoadDefaults()
+        {
+            largeServer = false;
+            gameName = "GameMode";
+            hideWorkshop = false;
+            hideConfiguration = false;
+            rocketPlugins = new List<string>()
+            {
+                "Please give credit to every developer that has a plugin on this server",
+                "Thank you",
+                "xdlewisdx",
+            };
+        }
+    }
+}
